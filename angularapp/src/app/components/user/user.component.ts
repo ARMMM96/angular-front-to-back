557ecebb8eda0189422b30eeac6c02d58131b9b8
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2021-07-29 07:03:56 +0200

    Properties & Methods

--- angularapp/src/app/components/user/user.component.ts
+++ angularapp/src/app/components/user/user.component.ts
@@ -4,13 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
-  styles: [
-    `
-      h1 {
-        color: lightblue;
-      }
-    `,
-  ],
-  // template: '<h2>JohnDoe </h2>',
 })
-export class UserComponent {}
+export class UserComponent {
+  //Properties
+  firstName = 'John';
+  lastName = 'doe';
+  age = 30;
+
+  // Methods
+  constructor() {
+    // this.sayHello();
+    console.log(this.age);
+    this.hasBirthday();
+    console.log(this.age);
+  }
+
+  sayHello() {
+    console.log(`
+      Hello ${this.firstName}
+      ${this.lastName}
+       `);
+  }
+
+  hasBirthday() {
+    this.age += 1;
+  }
+}
