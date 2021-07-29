434522405fd1a26af55de28c39c177ad75b689b0
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2021-07-29 08:48:48 +0200

    C:/Program Files/Git/user.component.html

--- angularapp/src/app/components/user/user.component.ts
+++ angularapp/src/app/components/user/user.component.ts
@@ -7,19 +7,54 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
   //Properties
-  firstName = 'Ahmed';
-  lastName = 'doe';
-  age = 30;
-  address = {
-    street: '50 Main st',
-    city: 'Boston',
-    state: 'MA',
-  };
+  firstName: string;
+  lastName: string;
+  age: number;
+  address: object;
+
+  foo: any;
+
+  hasKids: boolean;
+  numberArray: number[];
+  stringArray: string[];
+  mixedArray: any[];
+  myTuple: [string, number, boolean];
+  unsable: void;
+  u: undefined;
+  n: null;
 
   // Methods
-  constructor() {}
+  constructor() {
+    this.firstName = 'John';
+    this.lastName = 'Doe';
+    this.age = 30;
+    this.address = {
+      street: '50 Main st',
+      city: 'Boston',
+      state: 'MA',
+    };
+
+    this.foo = '1';
+    this.hasKids = true;
+    this.numberArray = [123, 123];
+    this.stringArray = [
+      'hello',
+      'hello',
+      'let me tell you what is like to be a zero zero',
+    ];
+    this.mixedArray = [123, 'hello'];
+    this.myTuple = ['hl', 99, false];
+    this.unsable = undefined;
+    this.u = undefined;
+    this.n = null;
+    console.log(this.addNumbers(1, 2));
+  }
 
   showAge() {
     return this.age;
+  }
+
+  addNumbers(num1: number, num2: number): number {
+    return num1 + num2;
   }
 }
