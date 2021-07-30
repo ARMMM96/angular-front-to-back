3975d2d429abb74bdecf2eec0eaffbad45307142
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2021-07-30 14:13:49 +0200

    Loops With ngFor

--- angularapp/src/app/components/users/users.component.ts
+++ angularapp/src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
-  constructor() {}
+  users: User[];
 
+  constructor() {
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'MA',
+        },
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+          street: '20 School st',
+          city: 'Lynn',
+          state: 'MA',
+        },
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+          street: '55 Mill st',
+          city: 'Miami',
+          state: 'FL',
+        },
+      },
+    ];
+  }
   ngOnInit(): void {
-    console.log('init ...');
+    this.addUser({
+      firstName: 'Ahmed',
+      lastName: 'Rabie',
+      age: 26,
+      address: {
+        street: '55 Mill st',
+        city: 'Miami',
+        state: 'FL',
+      },
+    });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 }
