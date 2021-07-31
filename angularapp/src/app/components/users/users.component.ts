f7f07ac252d62fd9c7635a69ef3973949314d659
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2021-07-31 08:28:13 +0200

    Component & Template Clean Up

--- angularapp/src/app/components/users/users.component.ts
+++ angularapp/src/app/components/users/users.component.ts
@@ -11,8 +11,6 @@ export class UsersComponent implements OnInit {
   showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
-  currentClasses = {};
-  currentStyles = {};
 
   constructor() {}
   ngOnInit(): void {
@@ -26,9 +24,7 @@ export class UsersComponent implements OnInit {
           city: 'Boston',
           state: 'MA',
         },
-        image: 'http://lorempixel.com/600/600/people/1',
         isActive: true,
-        balance: 100,
         registered: new Date('01/02/2018 08:30:00'),
       },
       {
@@ -40,9 +36,7 @@ export class UsersComponent implements OnInit {
           city: 'Lynn',
           state: 'MA',
         },
-        image: 'http://lorempixel.com/600/600/people/2',
         isActive: false,
-        balance: 160,
         registered: new Date('01/12/2017 06:20:00'),
       },
       {
@@ -54,32 +48,15 @@ export class UsersComponent implements OnInit {
           city: 'Miami',
           state: 'FL',
         },
-        image: 'http://lorempixel.com/600/600/people/5',
         isActive: true,
-        balance: 500,
         registered: new Date('11/02/2016 10:30:00'),
       },
     ];
     this.loaded = true;
-    this.setCurrentClasses();
-    this.setCurrentStyles();
   }
 
   addUser(user: User) {
     this.showExtended = true;
     this.users.push(user);
-  }
-  setCurrentClasses() {
-    this.currentClasses = {
-      'btn-success': this.enableAdd,
-      'big-text': this.showExtended,
-    };
-  }
-
-  setCurrentStyles() {
-    this.currentStyles = {
-      'padding-top': this.showExtended ? '0' : '50px',
-      'font-size': this.showExtended ? '' : '30px',
-    };
   }
 }
--- angularapp/src/app/models/User.ts
+++ angularapp/src/app/models/User.ts
@@ -7,8 +7,6 @@ export interface User {
     city?: string;
     state?: string;
   };
-  image?: string;
   isActive?: boolean;
-  balance?: number;
   registered?: any;
 }
