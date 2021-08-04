da16e6b5fbb01bc67ecc9b64347ff0ad2a38ade3
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2021-08-04 05:16:32 +0200

    Create App Routing Module

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { UserService } from './services/user.service';
 import { PostService } from './services/post.service';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostFormComponent } from './components/post-form/post-form.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { PostFormComponent } from './components/post-form/post-form.component';
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [UserService, PostService],
   bootstrap: [AppComponent],
