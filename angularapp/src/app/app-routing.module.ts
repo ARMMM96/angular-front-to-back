5f9d799f24c1aded67aa274484322264fce5ca31
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2021-08-04 05:30:20 +0200

    Creating & Mapping Routes

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+import { PostsComponent } from './components/posts/posts.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'posts', component: PostsComponent },
+];
 
 @NgModule({
   exports: [RouterModule],
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { PostService } from './services/post.service';
 import { PostsComponent } from './components/posts/posts.component';
 import { PostFormComponent } from './components/post-form/post-form.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { AppRoutingModule } from './app-routing.module';
     NavbarComponent,
     PostsComponent,
     PostFormComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
